021d64b0ffa2f5e8a2f29a590a0267eaa76d23a0
Brandon Jun <arachnite.b@gmail.com>
2025-03-07 09:41:37 -0800

    Added main logic and constants back in

--- src/main/java/io/github/arachnite/Main.java
+++ src/main/java/io/github/arachnite/Main.java
@@ -1,7 +1,14 @@
 package io.github.arachnite;
 
+import io.github.arachnite.logic.Board;
+import io.github.arachnite.logic.Player;
+import io.github.arachnite.logic.StartScreen;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Board[][] tictactoeBoard = new Board[3][3];
+        Player player1 = new Player();
+        Player player2 = new Player();
+        StartScreen.printStartMessage(tictactoeBoard, player1, player2);
     }
 }
